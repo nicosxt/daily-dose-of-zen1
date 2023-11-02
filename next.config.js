dd1ec4dda03cc321a1a60f2b51b3dd1be9a46052
Jonathan Miller <jon@jmill.dev>
2023-11-02 09:22:03 -0400

    init

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        hostname: "edenartlab-prod-data.s3.us-east-1.amazonaws.com",
+      },
+      {
+        hostname: "edenartlab-prod-data.s3.us-east-1.amazonaws.com",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

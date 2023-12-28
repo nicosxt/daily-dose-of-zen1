9ef8c1614f72f4c41823ed98ed3387b42afb0917
Jonathan Miller <jon@jmill.dev>
2023-12-27 20:58:11 -0800

    simplify

--- next.config.js
+++ next.config.js
@@ -5,9 +5,11 @@ const nextConfig = {
     remotePatterns: [
       {
         hostname: "edenartlab-prod-data.s3.us-east-1.amazonaws.com",
+        hostname: "edenartlab-stage-data.s3.us-east-1.amazonaws.com",
       },
       {
         hostname: "edenartlab-prod-data.s3.us-east-1.amazonaws.com",
+        hostname: "edenartlab-stage-data.s3.us-east-1.amazonaws.com",
       },
     ],
   },
--- src/pages/api/create.ts
+++ src/pages/api/create.ts
@@ -9,45 +9,13 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       apiKey: process.env.EDEN_API_KEY,
       apiSecret: process.env.EDEN_API_SECRET,
     });
-    const response = await eden.tasks.create({
+    const uris = await eden.create({
       generatorName: "create",
       config: {
         text_input,
       },
     });
-    const taskId = response.taskId;
-
-    if (!taskId) {
-      return res.status(500).json({ error: "Task creation failed" });
-    }
-
-    // Poll for result
-    const startTime = Date.now();
-    let taskResult;
-    while (Date.now() - startTime < 10 * 60 * 1000) {
-      // 10 minutes timeout
-      const taskResponse = await eden.tasks.get({ taskId });
-      const task = taskResponse.task;
-      if (!task) {
-        return res.status(500).json({ error: "Task not found" });
-      }
-      if (task.status === "completed") {
-        taskResult = task.creation?.uri;
-        break;
-      } else if (task.status === "failed") {
-        return res.status(500).json({ error: "Task failed" });
-      }
-      await new Promise((resolve) => setTimeout(resolve, 5000)); // wait for 5 seconds before next poll
-    }
-
-    if (!taskResult) {
-      return res
-        .status(500)
-        .json({ error: "Task timed out or something went wrong" });
-    }
-
-    // use taskResult here
-    return res.status(200).json({ uri: taskResult });
+    return res.status(200).json({ uri: uris[0] });
   } catch (error) {
     console.error(error);
     return res.status(500).json({ error: "Internal server error" });

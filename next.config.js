ac4dd4f2a2b6482f72c6fc7e64596569bd007473
Jonathan Miller <jon@jmill.dev>
2024-01-02 20:05:14 -0800

    bump

--- next.config.js
+++ next.config.js
@@ -4,11 +4,11 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
+        protocol: "https",
         hostname: "edenartlab-prod-data.s3.us-east-1.amazonaws.com",
-        hostname: "edenartlab-stage-data.s3.us-east-1.amazonaws.com",
       },
       {
-        hostname: "edenartlab-prod-data.s3.us-east-1.amazonaws.com",
+        protocol: "https",
         hostname: "edenartlab-stage-data.s3.us-east-1.amazonaws.com",
       },
     ],
